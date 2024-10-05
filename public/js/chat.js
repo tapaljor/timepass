9dda49d722d75a0d4c2e880732e8799af5650c9a
Tashi Paljor <tapaljor@gmail.com>
2024-10-05 14:28:23 -0400

    Oct 5 @1428

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://timepass-2af3f609bb70.herokuapp.com/:3000');
+var socket = io.connect('https://timepass-2af3f609bb70.herokuapp.com');
 
 //receiving msg through socket/server
 socket.on('updatedUser', (data) => {
