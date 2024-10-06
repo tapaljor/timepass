f8983714a3e519541fbb8773236c78b70e99f592
Tashi Paljor <tapaljor@gmail.com>
2024-10-05 22:04:43 -0400

    Oct 5 @2205

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const sessionMiddleware = session({
     saveUninitialized: true,
     cookie: {
         sameSite: 'Lax', 
-        secure: false,
+        secure: true,
         maxAge: 600000
     }
 });
